f9fc4319b20a8105c9da8ea6f63c0d1de7361d95
njtzion <njtzion@gmail.com>
2025-06-07 19:33:25 +1000

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Callback from "./pages/Callback";
 
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Callback from './pages/Callback';
-import Portfolio from './pages/Portfolio';
-
-export default function App() {
+function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/callback" element={<Callback />} />
         <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/callback" element={<Callback />} />
       </Routes>
     </Router>
   );
 }
+
+export default App;
