d4702637c849cf373dab2e5adb1b3723401a551d
njtzion <njtzion@gmail.com>
2025-06-07 19:32:53 +1000

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,7 @@
-module.exports = { content: ['./index.html', './src/**/*.{js,jsx}'], theme: { extend: {} }, plugins: [] }
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
